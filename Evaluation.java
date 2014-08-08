2af0407ef527a0cac5db16fd289a021f95c95411
luhaolu <therealhaolu@gmail.com>
2014-08-08 10:24:39 -0700

    working hybird model and evaluation

--- Evaluation.java
+++ Evaluation.java
@@ -1,152 +1,114 @@
 package csc485project;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 
 public class Evaluation {
 
-	// 10 fold cross validation using RMSE, precision and recall. We categorize rating "1-3" as irrelevant and
+	// evaluation using RMSE, precision and recall. We categorize rating "1-3" as irrelevant and
 	// "4-5" as relevant
-	public double tenFoldRMSE(int[][] table, List<Integer> uniqueMoives,
-			int mode) throws IOException {
-
-		int totalElement = table.length * table[0].length - emptyElement(table);
-		int[][] tablePredicted;
-		int[] predictedArray;
-		List<int[]> predictedPostions;
-
-		int foldSize = totalElement / 10;
-		int blankedCount;
-
-		int tableIndexX = 0;
-		int tableIndexY = 0;
-
-		int[] actulValues;
-		int actulValuesIndex;
+	public void evaluate(String fileName ) throws IOException {
 		
-		int TP = 0;
+		int [][] predictedTable = new int [943][1682];
+		BufferedReader br = new BufferedReader(new FileReader(fileName));
+		String line = br.readLine();
+		int lineNumber = 0;
+	    while (line != null) {
+	    	
+	    	//Skip the first line
+	    	if (lineNumber==0) {
+	    		lineNumber++;
+	    		line = br.readLine();
+	    		continue;
+	    	}
+	    	String[] row = line.split(",\\s*");
+	    	for (int i=1;i<row.length;i++) {
+	    		predictedTable[lineNumber-1][i-1] = Integer.parseInt(row[i]);
+	    	}
+	    	lineNumber++;
+	    	line = br.readLine();
+	    }
+	    br.close();
+	    
+	    int [][] originalTable = new int [943][1682];
+		BufferedReader br2 = new BufferedReader(new FileReader("origian_data.csv"));
+		String line2= br2.readLine();
+		int lineNumber2 = 0;
+	    while (line2 != null) {
+	    	
+	    	//Skip the first line
+	    	if (lineNumber2==0) {
+	    		lineNumber2++;
+	    		line2 = br2.readLine();
+	    		continue;
+	    	}
+	    	String[] row = line2.split(",\\s*");
+	    	for (int i=1;i<row.length;i++) {
+	    		originalTable[lineNumber2-1][i-1] = Integer.parseInt(row[i]);
+	    	}
+	    	lineNumber2++;
+	    	line2 = br2.readLine();
+	    }
+	    br2.close();
+	    
+	    int TP = 0;
 		int FP = 0;
 		int TN = 0;
 		int FN = 0;
-
-		double[] sqrMeanArray = new double[10];
-
-		for (int foldCount = 0; foldCount < 10; foldCount++) {
-			// displayTable(table);
-			tablePredicted = copyTable(table);
-			blankedCount = 0;
-			actulValuesIndex = 0;
-			predictedPostions = new ArrayList<int[]>();
-			actulValues = new int[foldSize];
-			predictedArray = new int[foldSize];
-
-			// Blank out the element in the subset
-			outerloop: while (tableIndexX < table.length) {
-				while (tableIndexY < table[0].length) {
-
-					if (table[tableIndexX][tableIndexY] == 0) {
-						tableIndexY++;
-						continue;
-
-					}
-
-					// store the actual value in an array
-					actulValues[actulValuesIndex] = table[tableIndexX][tableIndexY];
-					actulValuesIndex++;
-
-					tablePredicted[tableIndexX][tableIndexY] = 0;
-
-					// store the position in an array, and put it in a list
-					int[] positon = { tableIndexX, tableIndexY };
-					predictedPostions.add(positon);
-
-					tableIndexY++;
-					blankedCount++;
-
-					if (blankedCount == foldSize) {
-						break outerloop;
-					}
-				}
-				tableIndexY = 0;
-				tableIndexX++;
-			}
-
-			// Run Hybrid model
-			if (mode == 0) {
-				tablePredicted = new Hybrid().compute(tablePredicted,table);
-			}
-
-			// Run Pearson
-			else if (mode == 1) {
-				tablePredicted = new NewPearson().compute(tablePredicted);
-
-			}
-
-			// Run Base Line
-			else if (mode == 2) {
-				new BaseLine().compute(tablePredicted);
-			}
-
-			//displayTable(tablePredicted,table);
-			//System.out.println();
-
-			// Store the recommended rating in an array
-			int predictedArrayIndex = 0;
-			for (int[] position : predictedPostions) {
-
-				predictedArray[predictedArrayIndex] = tablePredicted[position[0]][position[1]];
-				predictedArrayIndex++;
-
-			}
-
-			int sumSqrDiff = 0;
-			
-			for (int i = 0; i < foldSize; i++) {
-				sumSqrDiff = (int) (Math.pow(
-						actulValues[i] - predictedArray[i], 2) + sumSqrDiff);
-				
-				// compute precision and recall
-				// count number of relevant in ground truth table
-				if (actulValues[i] > 3) {
+		int count = 0;
+		int sum = 0;
+		for (int i = 0; i < originalTable.length; i++) {
+			for (int j = 0; j < originalTable[0].length; j++) {
+				if (originalTable[i][j] != 0) {
 					
-					// Recommendation predicted the item as relevant as ground
-					// truth
-					if (predictedArray[i] > 3) {
-						TP++;
-					}
-					// Recommendation predicted the item as irrelevant,
-					// different from ground truth
-					else {
-						FN++;
-					}
+					//compute the error for RMSE
+					int diff = predictedTable[i][j] - originalTable[i][j];
+					diff = (int) Math.pow(diff, 2);
+					sum = sum + diff;
+					count++;
 					
-				} else {
-					
-					// Recommendation predicted the item as relevant, different
-					// from ground truth
-					if (predictedArray[i] > 3) {
-						FP++;
-					}
-					
-					// Recommendation predicted the item as irrelevant as ground
-					// truth
-					else {
-						TN++;
+					// compute precision and recall
+					// count number of relevant in ground truth table
+					if (originalTable[i][j] > 3) {
+						
+						// Recommendation predicted the item as relevant as ground
+						// truth
+						if (predictedTable[i][j] > 3) {
+							TP++;
+						}
+						// Recommendation predicted the item as irrelevant,
+						// different from ground truth
+						else {
+							FN++;
+						}
+						
+					} else {
+						
+						// Recommendation predicted the item as relevant, different
+						// from ground truth
+						if (predictedTable[i][j] > 3) {
+							FP++;
+						}
+						
+						// Recommendation predicted the item as irrelevant as ground
+						// truth
+						else {
+							TN++;
+						}
+						
 					}
 					
 				}
-
 			}
-
-			double mean = (double) sumSqrDiff / foldSize;
-			double sqrMean = Math.sqrt(mean);
-			sqrMeanArray[foldCount] = sqrMean;
-			
 		}
+		double mean = (double) sum/count;
+		double RMSE = Math.sqrt(mean);
 		double precision = (double) TP / (TP + FP);
 		double recall = (double) TP / (TP + FN);
 		double accurcy = (double) (TP + TN) / (TP + TN + FN + FP);
@@ -154,57 +116,9 @@ public class Evaluation {
 		System.out.println("precision: " + precision);
 		System.out.println("recall: " + recall);
 		System.out.println("accurcy: " + accurcy);
-
-		return computeAvg(sqrMeanArray);
-	}
-
-	public double computeAvg(double[] sqrMeanArray) {
-
-		double sum = 0.0;
-		for (int i = 0; i < sqrMeanArray.length; i++) {
-
-			sum = sum + sqrMeanArray[i];
-
-		}
-
-		return sum / sqrMeanArray.length;
-	}
-
-	public int emptyElement(int[][] table) {
-
-		int totalEmpty = 0;
-		for (int i = 0; i < table.length; i++) {
-			for (int j = 0; j < table[0].length; j++) {
-				if (table[i][j] == 0) {
-					totalEmpty++;
-				}
-			}
-		}
-
-		return totalEmpty;
-	}
-
-	public int[][] copyTable(int[][] table) {
-
-		int[][] copiedTable = new int[table.length][table[0].length];
-
-		for (int i = 0; i < table.length; i++) {
-			for (int j = 0; j < table[0].length; j++) {
-				copiedTable[i][j] = table[i][j];
-			}
-		}
-
-		return copiedTable;
-	}
-
-	// display preditcted table and original table for debuging
-	public void displayTable(int[][] tablePredicted, int[][] table) {
-		for (int i = 0; i < tablePredicted.length; i++) {
-			for (int j = 0; j < tablePredicted[0].length; j++) {
-				System.out.print(tablePredicted[i][j] + ""+table[i][j] + " ");
-			}
-			System.out.println();
-		}
+		System.out.println("RMSE: " + RMSE);
+		
+		
 	}
 
 	public static void main(String[] args) throws IOException {
@@ -218,14 +132,13 @@ public class Evaluation {
 		int[][] table = parser.toTable(userMap, uniqueMoives);
 
 		Evaluation eva = new Evaluation();
-
-		System.out.println("Pearson Results");
-		System.out.println("RMSE: " + eva.tenFoldRMSE(table, uniqueMoives, 1) + "\n");
-
 		System.out.println("BaseLine");
-		System.out.println("RMSE: " + eva.tenFoldRMSE(table, uniqueMoives, 2) + "\n");
-
-		System.out.println("Hybrid");
-		System.out.println("RMSE: " + eva.tenFoldRMSE(table, uniqueMoives, 0) + "\n");
+		eva.evaluate("baseline_results.csv");
+		
+		System.out.println("Pearson Results");
+		eva.evaluate("Pearson_results.csv");
+		
+		System.out.println("Hybrid pearson first");
+		eva.evaluate("Hybrid_pearson_first.csv");
 	}
 }
--- Hybrid.java
+++ Hybrid.java
@@ -1,6 +1,11 @@
 package csc485project;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Iterator;
@@ -10,24 +15,105 @@ import java.util.Map.Entry;
 
 public class Hybrid {
 
-	public int[][] compute(int[][] tablePredicted, int[][] table) {
+	public int[][] computePearsonFirst() throws IOException {
 
-		tablePredicted = new NewPearson().compute(tablePredicted);
-		BaseLine baseline = new BaseLine();
-		baseline.compute(tablePredicted);
+		//load the Pearson results file too save time
+		int [][] predictedTable = new int [943][1682];
+		BufferedReader br = new BufferedReader(new FileReader("Pearson_results.csv"));
+		String line = br.readLine();
+		int lineNumber = 0;
+	    while (line != null) {
+	    	
+	    	//Skip the first line
+	    	if (lineNumber==0) {
+	    		lineNumber++;
+	    		line = br.readLine();
+	    		continue;
+	    	}
+	    	String[] row = line.split(",\\s*");
+	    	for (int i=1;i<row.length;i++) {
+	    		predictedTable[lineNumber-1][i-1] = Integer.parseInt(row[i]);
+	    	}
+	    	lineNumber++;
+	    	line = br.readLine();
+	    }
+	    br.close();
+	    
+	    //then use Baseline
+	    //predictedTable = new NewPearson().compute(predictedTable);
+	    BaseLine baseline = new BaseLine();
+		baseline.compute(predictedTable);
 
-		// recover the ground truth
-		for (int i = 0; i < tablePredicted.length; i++) {
-			for (int j = 0; j < tablePredicted[i].length; j++) {
-				if (table[i][j] != 0) {
-					tablePredicted[i][j] = table[i][j];
-				}
+		return predictedTable;
+	}
+	
+	public int[][] computeBaselineFirst() throws IOException {
+
+		//load the baseline results file too save time
+		int [][] predictedTable = new int [943][1682];
+		BufferedReader br = new BufferedReader(new FileReader("baseline_results.csv"));
+		String line = br.readLine();
+		int lineNumber = 0;
+	    while (line != null) {
+	    	
+	    	//Skip the first line
+	    	if (lineNumber==0) {
+	    		lineNumber++;
+	    		line = br.readLine();
+	    		continue;
+	    	}
+	    	String[] row = line.split(",\\s*");
+	    	for (int i=1;i<row.length;i++) {
+	    		predictedTable[lineNumber-1][i-1] = Integer.parseInt(row[i]);
+	    	}
+	    	lineNumber++;
+	    	line = br.readLine();
+	    }
+	    br.close();
+	    
+	    //then use Pearson
+	    predictedTable = new NewPearson().compute(predictedTable);
+
+		return predictedTable;
+	}
+	
+	public void writeToFile(HashMap<Integer, ArrayList<Item>> userMap,
+			List<Integer> uniqueMoives, int[][] table)
+			throws FileNotFoundException, UnsupportedEncodingException {
+
+		PrintWriter writer = new PrintWriter("Hybrid_pearson_first.csv", "UTF-8");
+		writer.print(" ,");
+		// save the movie id in file
+		Iterator<Integer> movieIterator = uniqueMoives.iterator();
+		while (movieIterator.hasNext()) {
+			int movieId = movieIterator.next();
+			writer.print(movieId + ", ");
+		}
+		writer.println();
+
+		Iterator<Integer> iterator = userMap.keySet().iterator();
+
+		for (int i = 0; i < table.length; i++) {
+			int userId = iterator.next();
+			writer.print(userId + ", ");
+			for (int j = 0; j < table[0].length; j++) {
+				writer.print(table[i][j] + ", ");
 			}
+			writer.println();
 		}
 
-		return tablePredicted;
+		writer.close();
+
 	}
 
 	public static void main(String[] args) throws IOException {
+		HashMap userMap = new HashMap();
+		DataParser parser = new DataParser();
+		List<Integer> uniqueMoives = new ArrayList<Integer>();
+		parser.parse(userMap, uniqueMoives);
+		int[][] table = parser.toTable(userMap, uniqueMoives);
+		
+		int[][] prediction = new Hybrid().computePearsonFirst();
+		new Hybrid().writeToFile(userMap, uniqueMoives, prediction);
 	}
 }
